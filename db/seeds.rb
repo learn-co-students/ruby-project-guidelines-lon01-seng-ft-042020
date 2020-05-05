1ed6f92ae50955833c122bd077fe287a0440a6fd
CharliePayne1 <charliepayne1101@gmail.com>
2020-05-05 11:24:52 +0100

    Database successfully seeded.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 require 'faker'
 
 20.times do User.create(name: Faker::Name.unique.name)
+end
 
 20.times do Developer.create(name: Faker::Name.unique.name,  language: Faker::ProgrammingLanguage.name, email: Faker::Internet.email)
+end
 
-20.times do Project.create(name: Faker::App.name, category: Faker::Job.field, developer_id: Developer.all.sample.id user_id: User.all.sample.id)
+20.times do Project.create(name: Faker::App.name, category: Faker::Job.field, developer_id: Developer.all.sample.id, user_id: User.all.sample.id)
+end
+
+puts "Successfully Seeded."
