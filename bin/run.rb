5ceef0db5f136ffd8c7618f7e4b499e9706c8455
Adao <sansonov@gmail.com>
2020-05-06 13:15:06 +0100

    together

--- bin/run.rb
+++ bin/run.rb
@@ -6,21 +6,6 @@ BY = File.read('lib/by.txt')
 WELCOME = File.read('lib/welcome.txt')
 
 
-system('clear')
-
-# spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :arrow_pulse)
-# spinner.auto_spin
-# sleep(5)
-# puts WELCOME
-# spinner.stop("  IMAGINE WE HAVE SOME MYSTERIOUS MUSIC PLAYING NOW!".yellow.bold)
-# sleep(5)
-
-# system('clear')
-# spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :bouncing_ball)
-# spinner.auto_spin
-# puts BY
-# sleep(6)
-# spinner.stop("  WISH WE HAD A REALLY COOL MUSIC... ;-D".yellow.bold)
 
 binding.pry
 
--- lib/cli_app.rb
+++ lib/cli_app.rb
@@ -83,7 +83,7 @@ class CliApp
     end
 
     def suspects_over_age?
-        answer = prompt.ask('   Tell over what age do you wanna see the suspects?')
+        answer = prompt.ask('   Suspects over what age would you like to see?')
         @spinner.auto_spin
         sleep(4)
         @prompt.ok(@d1.suspects_over_age(answer))
@@ -92,7 +92,7 @@ class CliApp
     end
 
     def suspects_by_location?
-        answer = prompt.ask('   Tell what city you wanna look for the suspects!')
+        answer = prompt.ask('   What city would you like to look for the suspects?')
         @spinner.auto_spin
         sleep(4)
         @prompt.ok(@d1.suspects_by_location(answer))
@@ -106,52 +106,59 @@ class CliApp
     end 
 
     def suspects_by_hair_color?
-        answer = prompt.ask('   What hair color are you looking for?')
-            @spinner.auto_spin
-            sleep(4)
-            @prompt.ok(@d1.suspects_by_hair_color(answer))
+        answer = prompt.ask('   What type of hair color are you looking for?')
+        @spinner.auto_spin
+        sleep(4)
+        @prompt.ok(@d1.suspects_by_hair_color(answer))
+        if @d1.suspects_by_hair_color(answer).length == 0
+            prompt.error("There are no suspects with this hair color!")
             @spinner.stop("! - DONE - !")
-            nil 
+            method
+        end
+        @spinner.stop("! - DONE - !")
+        nil 
     end 
 
     def drunk_people?
-        @prompt.ok("    These are people that drinks regulary!!!")
-            @spinner.auto_spin
-            sleep(4)
-            @prompt.ok(@d1.drunk_people)
-            @spinner.stop("  ! - DONE - !")
-            
-            nil 
+        @prompt.ok("    These are the regular drinkers!")
+        @spinner.auto_spin
+        sleep(4)
+        @prompt.ok(@d1.drunk_people)
+        @spinner.stop("  ! - DONE - !")    
+        nil 
     end 
 
     def into_tech?
-        @prompt.ok("    They are nerds!!!")
+        @prompt.ok("    These are the computer nerds!!!")
         @spinner.auto_spin
         sleep(4)
         @prompt.ok(@d1.into_tech)
         @spinner.stop("  ! - DONE - !")
-        
         nil 
     end 
 
     def speaks_a_lot?
-        @prompt.ok("    Those who speaks more than one language")
-            @spinner.auto_spin
-            sleep(4)
-            @prompt.ok(@d1.speaks_a_lot)
-            @spinner.stop("  ! - DONE - !")
-            
-            nil 
+        @prompt.ok("    These guys speak more than one language!")
+        @spinner.auto_spin
+        sleep(4)
+        @prompt.ok(@d1.speaks_a_lot)
+        @spinner.stop("  ! - DONE - !") 
+        nil 
     end 
 
     def one_thing_about_me?
-        answer = prompt.ask('   What person do you wanna know about?')
+        answer = prompt.ask('   What person do you wanna know about? (Type in full name)')
         @spinner.auto_spin
         sleep(4)
+        if Suspect.find_by(name: answer)
         @prompt.ok(@d1.one_thing_about_me(answer))
-        @spinner.stop("  ! - DONE - !")
-        
+        @spinner.stop("  ! - DONE - !") 
         nil
+        else
+        @spinner.stop("  ! - DONE - !") 
+        prompt.error("Incorrect name!")    
+        one_thing_about_me?
+        end
     end 
 
     def suspects_by_gender?         
@@ -159,8 +166,13 @@ class CliApp
         @spinner.auto_spin
         sleep(4)
         @prompt.ok(@d1.suspects_by_gender(answer))
-        @spinner.stop("  ! - DONE - !")
-        nil
+        if @d1.suspects_by_gender(answer).length == 0
+            prompt.error("There are no suspects with that gender!")
+            @spinner.stop("! - DONE - !")
+            method
+        end
+        @spinner.stop("! - DONE - !")
+        nil 
     end 
 
 
@@ -171,6 +183,10 @@ class CliApp
             if choice
                  method
             end
+        choice2 = @prompt.yes?("Do you want to use another filter?") 
+            if choice2
+                method
+            end
         sleep(2)
         delete_suspect
         sleep(2)
@@ -180,11 +196,9 @@ class CliApp
     end
 
     def game
-        puts WELCOME
-        sleep(5)
-        puts BY
-        sleep(3)
         intro
+        beginning
+        part_1
         turn
         part_2
         turn
@@ -195,8 +209,7 @@ class CliApp
         part_5
         turn
         part_6
-        #tell more story
-        #turn
+        #end win/lose
     end
 
     def time
@@ -204,59 +217,102 @@ class CliApp
         time.strftime('%H:%M:%S, %a %d %b %Y')
     end
 
+    
+
     def intro
-        puts "Please enter your name to begin the game."
-        username = gets.chomp
-        puts "Welcome, Detective #{username}. There has been a theft at Flatiron School."
-        puts ""
-        puts "We know very little. Only that an important piece of tech was stolen from the campus, \n sometime in the last couple of hours. You are the first person to arrive at the crime \n scene at #{time}, but your Sergeant Chief has already rounded up a \n list of 15 suspects that have been known for this kind of theft before. These guys are \n renowned, the \e[36mStandard Deviants\e[0m of this kind of technological savvy. You are the \e[36mMethod Man\e[0m. \n
+        system('clear')
+        spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :arrow_pulse)
+        spinner.auto_spin
+        sleep(5)
+        puts WELCOME
+        spinner.stop("  IMAGINE WE HAVE SOME MYSTERIOUS MUSIC PLAYING NOW!".yellow.bold)
+        sleep(5)
+
+        system('clear')
+        spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :bouncing_ball)
+        spinner.auto_spin
+        puts BY
+        sleep(6)
+        spinner.stop("  WISH WE HAD A REALLY COOL MUSIC... ;-D".yellow.bold)
+        nil
+    end
+
+    def beginning
+        system('clear')
+        username = prompt.ask("Please enter your name to begin the game.")
+        puts "\n"
+        puts "  Welcome, Detective #{username}. There has been a theft at Flatiron School."
+        puts "\n"
+        puts "  We know very little. Only that an important piece of tech was stolen from the campus, \n sometime in the last couple of hours. You are the first person to arrive at the crime \n scene at #{time}, but your Sergeant Chief has already rounded up a \n list of 15 suspects that have been known for this kind of theft before. These guys are \n renowned, the \e[36mStandard Deviants\e[0m of this kind of technological savvy. You are the \e[36mMethod Man\e[0m. \n
         It’s your job to suss out the culprit."
-        choice = @prompt.yes?("Begin?")
-        if choice
-            part_1
-        end
+        @prompt.keypress("Press to continue...")
+        puts "\n"
     end
 
     def part_1
-        puts "A thick fog permeates the London streets as you go over evidence from the case. \n The crime was committed right here, in London, so immediately you can strike \n off suspects that weren’t in the country."
+        puts "  A thick fog permeates the London streets as you go over evidence from the case. \n The crime was committed right here, in London, so immediately you can strike \n off suspects that weren’t in the country."
     end
 
     def part_2
-        puts "You also know that the suspect must’ve been really into tech to attempt such an extraordinary feat."
+        puts "  You also know that the suspect must’ve been really into tech to attempt such an extraordinary feat."
     end
 
     def part_3
-        puts "Good. Now your investigations are under way. Your foot crunches down on the shattered \n glass as you make your way into the building, but you pause just as you reach the door. \n Something catches your eye. There’s a Tamagotchi lying on the floor. You pick it up. \n It looks important."
+        puts "  Good. Now your investigations are under way. Your foot crunches down on the shattered \n glass as you make your way into the building, but you pause just as you reach the door. \n Something catches your eye. There’s a Tamagotchi lying on the floor. You pick it up. \n It looks important."
         puts ""
-        sleep(2)
-        puts "The building had been closed for renovations for a few months now, so you tread with caution \n through the darkness, side-stepping pipes and displaced concrete. You check for movement, perhaps \n the thief is still lurking in the shadows, but you realise it’s a classic \e[36m404\e[0m: no-ones there."
+        sleep(4)
+        @prompt.keypress("Press to continue...")
+        puts "\n"
+        puts "  The building had been closed for renovations for a few months now, so you tread with caution \n through the darkness, side-stepping pipes and displaced concrete. You check for movement, perhaps \n the thief is still lurking in the shadows, but you realise it’s a classic \e[36m404\e[0m: no-ones there."
         puts ""
-        sleep(2)
+        sleep(4)
         puts "\e[36mKeep Your Eyes on the Prys\e[0m, Detective."
         puts ""
         sleep(2)
-        puts "The smell of alcohol hits your nose. At first you pay no mind to it, but as you look closely, \n you can make something out at the far end of the room. It’s a half empty bottle of Bourbon. \n Interesting… Based on this new evidence, you choose to refine your suspect list even further."
+        @prompt.keypress("Press to continue...")
+        puts "\n"
+        puts "  The smell of alcohol hits your nose. At first you pay no mind to it, but as you look closely, \n you can make something out at the far end of the room. It’s a half empty bottle of Bourbon. \n Interesting… Based on this new evidence, you choose to refine your suspect list even further."
     end
 
     def part_4
+        @prompt.keypress("Press to continue...")
+        puts "\n"
         puts "The perpetrator must’ve been here long enough to drink most of their drink, which means they had \n been waiting around for a suspicious amount of time.  What were they waiting for? Any \e[36mtwo-bit Schema\e[0m \n knows not to hang around at the scene of the crime. You think back to the Tamagotchi you found at \n the start of the scene of the crime. What was anyone doing with a Tamagotchi at a crime scene?"
+        puts "\n"
+        @prompt.keypress("Press to continue...")
+        puts "\n"
         puts ""
+        sleep(2)
         puts "You hazard a guess and infer that your suspect must be under 30 and they must be really into tech."
     end
 
     def part_5
+        puts "\n"
+        @prompt.keypress("Press to continue...")
+        puts "\n"
         puts "\e[36mFork it\e[0m,  you think. There’s nothing left to be found here. You emerge from the building onto \n Finsbury Pavement. You are standing in the spill of a streetlight. To your right is Pret. \n To your left is Pret. And up ahead, sure as day, you see a Pret. Through the mist you see a \n quiet residential street, lined with newspapers. Something has been tossed onto the pavement – \n a scattered piece of paper, which you pick up and pocket."
+        sleep(2)
+        puts "\n"
+        @prompt.keypress("Press to continue...")
+        puts "\n"
+        sleep(2)
         puts ""
         puts "Your phone rings. Word just in from the \e[36mCSS and Desist\e[0m Neighbourhood Watch. A woman was seen \n fleeing the scene of the crime. Let’s refine our suspect list a little further."
     end
     
     def part_6
+        puts "\n"
+        @prompt.keypress("Press to continue...")
+        puts "\n"
         puts "You take another look at the paper you found on the floor. You focus your gaze and see subtle \n etchings on the envelope, as if someone had written something else on a paper on top of it. \n The nearly invisible scratches read…"
         puts ""
-        sleep(8)
+        sleep(6)
         puts "                 \e[95mI_Accidently_Drank_1lt_Energy_Drink\e[0m"
         puts ""
         sleep(3)
+        @prompt.keypress("Press to continue...")
+        sleep(2)
+        puts "\n"        
         puts "Time to make some deductions about this note and it's sender... Could it be… the \e[36mgitface killahs\e[0m, \n who have an affinity for monster drinks? No. Don't go \e[36mJSON Waterfalls\e[0m. \n You take another look at your suspect list and realise it can only be…"
     end
 
